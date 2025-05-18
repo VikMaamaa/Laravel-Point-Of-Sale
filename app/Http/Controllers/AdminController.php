677908ade368abc47c1e24e906807e6c00ab7a62
vikmaamaa <maamaavictor@yahoo.com>
2025-05-18 17:40:35 +0100

    feat: admin profile and image update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,5 +19,35 @@ class AdminController extends Controller
    public function AdminLogout() {
 
     return view('admin.admin_logout');
+   }//End Method
+
+   public function AdminProfile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile_view', compact('adminData'));
+   }//End Method
+
+   public function AdminProfileStore(Request $request) {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+
+        if($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_image/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_image'), $filename);
+            $data['photo'] = $filename;
+        }
+
+
+
+        $data->save();
+
+        return redirect()->back();
+
    }//End Method
 }
